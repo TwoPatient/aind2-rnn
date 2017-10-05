476ae57173542d7ede307a266280c8fecf10a8f1
Ben Carrier <ben.carrier@lineview.com>
2017-10-05 08:11:23 +0100

    Completed RNN Project

--- my_answers.py
+++ my_answers.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+import string
 
 
 # TODO: fill out the function below that transforms the input series 
@@ -12,6 +13,15 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+
+    i = 0
+    p = len(series)
+    t = window_size
+
+    while (i < p - t):
+        X.append(series[i:i + t])
+        y.append(series[i + t])
+        i += 1
 
     # reshape each 
     X = np.asarray(X)
@@ -23,12 +33,22 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1, activation='tanh'))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    ascii_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    unique_characters_in_text = set(text)
+
+    for character in unique_characters_in_text:
+        if character not in punctuation and character not in ascii_lowercase:
+            text = text.replace(character, ' ')
 
     return text
 
@@ -38,9 +58,21 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    i = 0
+    p = len(text)
+    t = window_size
+
+    while (i < p - t):
+        inputs.append(text[i:i+t])
+        outputs.append(text[i+t])
+        i += step_size
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size, num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
